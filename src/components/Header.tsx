6faca9b5f1ebe0b614e4409af1adc9de0eca5df1
santanarscs <raphaelstn@gmail.com>
2021-09-24 15:38:08 -0300

    add react scroll

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,19 @@
 import Link from 'next/link'
 import { MenuIcon } from '@heroicons/react/solid'
 import { useEffect, useState } from 'react'
+import { Link as LinkScroll } from 'react-scroll'
+
 function Header() {
+  
   const [scrolled, setScrolled] = useState(false)
+  const [activeLink, setActiveLink] = useState(null)
+  const [scrollActive, setScrollActive] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      setScrollActive(window.scroll > 20);
+    })
+  },[])
 
   useEffect(() => {
     window.onscroll = () => {
@@ -30,8 +41,80 @@ function Header() {
           </div>
         </div>
         <nav className="hidden sm:block justify-self-end">
-          <ul className="flex space-x-5 uppercase font-semibold tracking-wide text-gray-800">
-            <li>
+          <ul className="flex uppercase font-semibold tracking-wide text-gray-800">
+          <LinkScroll
+              activeClass="active"
+              to="home"
+              spy={true}
+              smooth={true}
+              duration={1000}
+              onSetActive={() => {
+                setActiveLink("home");
+              }}
+              className={
+                "px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative" +
+                (activeLink === "home"
+                  ? " text-orange-500 animation-active "
+                  : " text-black-500 hover:text-orange-500 a")
+              }
+            >
+              home
+            </LinkScroll>
+          <LinkScroll
+              activeClass="active"
+              to="services"
+              spy={true}
+              smooth={true}
+              duration={1000}
+              onSetActive={() => {
+                setActiveLink("services");
+              }}
+              className={
+                "px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative" +
+                (activeLink === "services"
+                  ? " text-orange-500 animation-active "
+                  : " text-black-500 hover:text-orange-500 a")
+              }
+            >
+              Serviços
+            </LinkScroll>
+            <LinkScroll
+              activeClass="active"
+              to="about"
+              spy={true}
+              smooth={true}
+              duration={1000}
+              onSetActive={() => {
+                setActiveLink("about");
+              }}
+              className={
+                "px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative" +
+                (activeLink === "about"
+                  ? " text-orange-500 animation-active "
+                  : " text-black-500 hover:text-orange-500 a")
+              }
+            >
+              Sobre mim
+            </LinkScroll>
+            <LinkScroll
+              activeClass="active"
+              to="contact"
+              spy={true}
+              smooth={true}
+              duration={1000}
+              onSetActive={() => {
+                setActiveLink("contact");
+              }}
+              className={
+                "px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative" +
+                (activeLink === "contact"
+                  ? " text-orange-500 animation-active "
+                  : " text-black-500 hover:text-orange-500 a")
+              }
+            >
+              Contato
+            </LinkScroll>
+            {/* <li>
               <Link href="#home">
                 <a>Home</a>
               </Link>
@@ -50,7 +133,7 @@ function Header() {
               <Link href="#contact">
                 <a>Contato</a>
               </Link>
-            </li>
+            </li> */}
           </ul>
         </nav>
         <button className="sm:hidden">
